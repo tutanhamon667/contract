40a06cfb44cad567643b4324b888d717f8d1f527
dzhiltcov <dzhiltcov@yandex.ru>
2023-09-17 20:32:38 +0200

    feat: сверстать секцию operation mode

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import "./Main.css";
 import React from "react";
 import StartWork from "../StartWork/StartWork";
+import OperationMode from "../OperationMode/OperationMode";
 
 function Main() {
   return (
     <main className="content">
       <StartWork />
+      <OperationMode />
     </main>
   );
 }
---
+++ frontend/src/components/OperationMode/OperationMode.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./OperationMode.css";
+
+function OperationMode() {
+  const [operationMode, setOperationMode] = useState(true);
+
+  const handleOperationMode = () => {
+    setOperationMode(!operationMode);
+  };
+
+  return (
+    <section className="operation-mode">
+      <button
+        className={
+          operationMode
+            ? `operation-mode__button operation-mode__button_projects operation-mode__button_action`
+            : `operation-mode__button operation-mode__button_projects`
+        }
+        onClick={handleOperationMode}
+      >
+        Список проектов
+      </button>
+      <button
+        className={
+          operationMode
+            ? `operation-mode__button operation-mode__button_freelance`
+            : `operation-mode__button operation-mode__button_freelance operation-mode__button_action`
+        }
+        onClick={handleOperationMode}
+      >
+        Фрилансеры
+      </button>
+    </section>
+  );
+}
+
+export default OperationMode;
