e3a41f1c50d866b1b55cb4fe7174db5afd8c531c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-19 14:39:53 +0300

    сверстать заглушку для NotFound

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Main from "../Main/Main";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Layout from "../../layout/layout";
+import NotFound from "../../pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Main/>}/>
+          {/* Тут будут защищенные роуты */}
+          {/* <Route element={<ProtectedRoute />}></Route> */}
+          <Route path="/" element={<Main />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
