0c78254d823b8fac3232027806d0d1c5c7060e4d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-19 14:04:14 +0300

    изменить формат компонентов с разметкой в jsx, подключить react-router-dom

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,16 +1,14 @@
-import "./App.css";
 import React from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <div className="app">
-        <div className="page">
+    <BrowserRouter>
           <Header />
           <Main />
-        </div>
-      </div>
+    </BrowserRouter>
   );
 }
 
