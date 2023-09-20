3c433cccba942f6a30b8b2781b426a1eef078e79
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-20 12:23:41 +0300

    часть верстки

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,40 +1,44 @@
 import React from "react";
-import styles from "./Auth.module.css";
+import "./Auth.css";
 
 const Auth = () => {
   return (
     <>
-      <div className={styles.wrapper}>
-        <h1 className={styles.title}>Регистрация</h1>
-        <div className={styles.register}>
-          <form className={styles.register__form}>
-            <div className={styles.register__statusInputs}>
-              <label>
-                <input
-                  className={styles.register__statusInput}
-                  type="radio"
-                  name="options"
-                  value="customer"
-                  // checked={selectedOption === 'option1'}
-                  // onChange={handleOptionChange}
-                />
-                Я заказчик
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="options"
-                  value="freelancer"
-                  // checked={selectedOption === 'option2'}
-                  // onChange={handleOptionChange}
-                />
-                Я фрилансер
-              </label>
+      <div className="wrapper">
+        <h1 className="title">Регистрация</h1>
+        <div className="register">
+          <form className="register__form">
+            <div className="register__statusNameContainer">
+              <div className="register__statusNameLeft">
+                <label>
+                  <input
+                    className="register__statusInput"
+                    type="radio"
+                    name="options"
+                    value="customer"
+                    // checked={selectedOption === 'option1'}
+                    // onChange={handleOptionChange}
+                  />
+                  Я заказчик
+                </label>
+                <input className="register__nameInput" type="text" />
+              </div>
+
+              <div className="register__statusNameRight">
+                <label>
+                  <input
+                    type="radio"
+                    name="options"
+                    value="freelancer"
+                    // checked={selectedOption === 'option2'}
+                    // onChange={handleOptionChange}
+                  />
+                  Я фрилансер
+                </label>
+              </div>
             </div>
-            <div className={styles.register__nameInputs}>
-            <input className={styles.register__nameInput} type="text" />
-            <input className={styles.register__nameInput} type="text" />
-            </div>
+
+            <input className="register__nameInput" type="text" />
           </form>
         </div>
       </div>
