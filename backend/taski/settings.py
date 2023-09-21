f19f1d9a75b26df96b8728ccd82ca7fc2c023e08
Ильгиз Нигматуллин <iznigmatullin@ya.ru>
2023-09-21 23:09:14 +0300

    added apps: api, orders, users. modified gitignore and setupcfg

--- backend/taski/settings.py
+++ backend/taski/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
