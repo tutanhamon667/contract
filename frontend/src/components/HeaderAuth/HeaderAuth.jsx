46c1cbef1088db7ec691856ec152cf64dca4a0ed
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-21 15:34:57 +0300

    научить хедер понимать, на какой странице она находится, для отображеня Регистрация/Вход

--- frontend/src/components/HeaderAuth/HeaderAuth.jsx
+++ frontend/src/components/HeaderAuth/HeaderAuth.jsx
@@ -6,16 +6,20 @@ function HeaderAuth() {
   const location = useLocation();
   return (
     <div className="header-auth">
-      {location.pathname !=='/signup' && <Link to="/signup">
-        <button className="header-auth__singup-button" type="button">
-          Регистрация
-        </button>
-      </Link>}
-      {location.pathname !=='/signin' && <Link to="/signin">
-        <button className="header-auth__entry-button" type="button">
-          Войти
-        </button>
-      </Link>}
+      {location.pathname !== "/signup" && (
+        <Link to="/signup">
+          <button className="header-auth__singup-button" type="button">
+            Регистрация
+          </button>
+        </Link>
+      )}
+      {location.pathname !== "/signin" && (
+        <Link to="/signin">
+          <button className="header-auth__entry-button" type="button">
+            Войти
+          </button>
+        </Link>
+      )}
     </div>
   );
 }
