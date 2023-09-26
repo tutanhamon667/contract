a0bb89d9fae68cc4535f5c044d35c7c0248fa2f5
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-26 22:45:53 +0300

    сверстать NotFoundPage по макету

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button"
 import './NotFound.css'
 
 
 const NotFound = () => {
   return (
     <div className="page">
-      <h1 className="title">
-        Такой страницы не существует
+      <h1 className="titleError">
+        404
       </h1>
+      <p className="descriptionError">Страница не найдена. Попробуйте вернуться назад или перейдите на главную.</p>
       <Link
         className="backLink"
         to="/"
       >
-        Вернуться на главную
+        <Button text='На главную' width={295} height={53} inheritTheme/>
       </Link>
     </div>
   );
