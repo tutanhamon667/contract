4ac531ab1a66b1bbebb90d5837aa87b4e8f83d18
Pavel Asafov <pavelaof@gmail.com>
2023-09-28 22:30:32 +0600

    сделать предварительную вёрстку

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer/Footer";
+// import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import  "./Layout.css";
 
@@ -11,7 +11,7 @@ export default function Layout({authenticated}) {
       <main className="outlet">
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
