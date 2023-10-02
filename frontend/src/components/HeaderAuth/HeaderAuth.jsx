171596162d12cf2063373a469666746cfe9143df
Pavel Asafov <pavelaof@gmail.com>
2023-10-02 20:07:11 +0600

    сверстать Header

--- frontend/src/components/HeaderAuth/HeaderAuth.jsx
+++ frontend/src/components/HeaderAuth/HeaderAuth.jsx
@@ -4,11 +4,13 @@ import "./HeaderAuth.css";
 
 function HeaderAuth() {
   const location = useLocation();
+  const regTextStyle = `header-auth__singup-button ${(location.pathname === "/") ? 'header-auth__singup-button_without-underline' : ''}`
+
   return (
     <div className="header-auth">
       {location.pathname !== "/signup" && (
         <Link to="/signup">
-          <button className="header-auth__singup-button" type="button">
+          <button className={regTextStyle} type="button">
             Регистрация
           </button>
         </Link>
@@ -18,11 +20,10 @@ function HeaderAuth() {
         location.pathname !== "/forgot-password" && (
           <Link to="/signin">
             <button
-              className={`${
-                location.pathname === "/signup"
+              className={`${location.pathname === "/signup"
                   ? "header-auth__singup-button"
                   : "header-auth__entry-button"
-              }`}
+                }`}
               type="button"
             >
               {location.pathname === "/signup" ? "Вход" : "Войти"}
