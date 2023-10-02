71081471b992b857c335dacaa24f5b1f25c1c059
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-02 03:15:41 +0300

    вернуть нагло убранные пропсы)

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import "./Button.css";
 
-
-const Button = ({ text, width, height, marginTop, marginBottom, buttonSecondary, buttonBlack, type, onClick, disabled }) => {
+const Button = ({
+  text,
+  width,
+  height,
+  marginTop,
+  marginBottom,
+  buttonSecondary,
+  buttonBlack,
+  buttonWhite,
+  type,
+  onClick,
+  disabled,
+  border,
+  fontSize,
+  fontWeight,
+  opacity
+}) => {
   return (
     <button
-      className={`button${buttonSecondary ? " buttonSecondary" : ""} button${buttonBlack ? " buttonBlack" : ""}`}
+      className={`button${buttonSecondary ? " buttonSecondary" : ""} button${
+        buttonBlack ? " buttonBlack" : ""
+      } ${
+        buttonWhite && " buttonWhite"}`}
       style={{
         width,
         height,
         marginTop,
-        marginBottom
+        marginBottom,
+        border,
+        fontSize,
+        fontWeight,
+        opacity
       }}
       type={type}
       onClick={onClick}
       disabled={disabled}
     >
-      <p className={`button__text${buttonSecondary ? " buttonSecondary__text" : ""}  button__text${buttonBlack ? " buttonBlack__text" : ""}`}>
+      <p
+        className={`button__text${
+          buttonSecondary ? " buttonSecondary__text" : ""
+        }  button__text${buttonBlack ? " buttonBlack__text" : ""}`}
+      >
         {text}
       </p>
     </button>
