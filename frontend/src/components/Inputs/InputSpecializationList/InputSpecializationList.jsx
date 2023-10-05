4f55be3b0fde080d3008192942fb93f5de8ca872
Pavel Asafov <pavelaof@gmail.com>
2023-10-05 13:51:50 +0600

    изменения регистра в Profile.css

--- frontend/src/components/Inputs/InputSpecializationList/InputSpecializationList.jsx
+++ frontend/src/components/Inputs/InputSpecializationList/InputSpecializationList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../../pages/Profiles/profile.css"
+import "../../../pages/Profiles/Profile.css"
 import "../../../pages/Profiles/ProfileFreelancer/ProfileFreelancer.css"
 import "./InputSpecializationList.css";
 
--- frontend/src/pages/Profiles/ProfileFreelancer/ProfileFreelancer.jsx
+++ frontend/src/pages/Profiles/ProfileFreelancer/ProfileFreelancer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 
-import "../profile.css";
+import "../Profile.css";
 import "./ProfileFreelancer.css";
 import "../../../components/Forms/FreelancerCompleteForm/FreelancerCompleteForm.css";
 
