b2282ed14af50bd0d639c2859a01fc6b2f724be9
sobbaka <a.bagaev1989@gmail.com>
2023-12-21 21:31:24 +0400

    add: сообщение, что поиск не дал результатов

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -77,6 +77,17 @@ function Card({ cards, isFirstTab }) {
     );
   }
 
+
+
+  if (cards.length === 0) {
+    console.log('No cards')
+    return <>
+      <h4>По вашему запросу ничего не найдено</h4>
+    </>
+  }
+
+  console.log(cards)
+
   return cards?.map(
     (data, index) => (
       // (freelanceFilter[`${data?.category}`] || !isFirstTab || one()) && (
