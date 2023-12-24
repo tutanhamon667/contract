1abc819f39d76832aabeade85b9aeb94342681c8
Ильгиз Нигматуллин <sleppg@gmail.com>
2023-12-24 22:51:16 +0300

    fix фильтров бюджета заданий

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,16 +1,30 @@
+from decimal import Decimal, DecimalException
+
+from django.core.exceptions import ValidationError
+from django.db.models import IntegerField, Q
+from django.db.models.functions import Cast
 from django_filters.rest_framework import FilterSet, NumberFilter, filters
 from django_filters.widgets import BooleanWidget
 
 from orders.models import Job, JobCategory, JobResponse
+from taski.settings import ASK_MSG
 
 
 class JobFilter(FilterSet):
     min_budget = NumberFilter(
-        field_name='budget', lookup_expr='gte', label='от'
+        field_name='budget',
+        lookup_expr='gte',
+        method='filter_min_budget',
+        label='от'
     )
+
     max_budget = NumberFilter(
-        field_name='budget', lookup_expr='lte', label='до'
+        field_name='budget',
+        lookup_expr='lte',
+        method='filter_max_budget',
+        label='до'
     )
+
     category = filters.ModelMultipleChoiceFilter(
         field_name='category__slug',
         to_field_name='slug',
@@ -29,6 +43,22 @@ class JobFilter(FilterSet):
         widget=BooleanWidget(),
     )
 
+    def filter_min_budget(self, queryset, name, value):
+        return self.filter_budget(queryset, value, 'gte')
+
+    def filter_max_budget(self, queryset, name, value):
+        return self.filter_budget(queryset, value, 'lte')
+
+    def filter_budget(self, queryset, value, lookup_expr):
+        try:
+            queryset = queryset.exclude(budget="Ожидает предложений").annotate(
+                int_budget=Cast('budget', IntegerField())
+            ).filter(**{f'int_budget__{lookup_expr}': int(value)})
+        except (ValueError, ValidationError):
+            pass  # Обработка ошибки преобразования в int
+
+        return queryset
+
     def get_is_responded(self, queryset, _, value):
         if value:
             return queryset.filter(
@@ -38,7 +68,7 @@ class JobFilter(FilterSet):
 
     class Meta:
         model = Job
-        fields = ('category', 'client',)
+        fields = ('category', 'client', 'min_budget', 'max_budget')
 
 
 class JobResponsesFilter(FilterSet):
